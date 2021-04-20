6b1a6bf172a952579efe4f61da447494952eb5b9
Yogesh Kumar Mangal <42840043+yogeshkumarmangal@users.noreply.github.com>
2021-04-20 23:13:02 +0530

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,34 @@
+import flask
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+data=pd.read_csv("Dashboards.csv")
+data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
+app = dash.Dash(__name__)
+app.layout = html.Div(
+    children=[
+        html.H1(children="Acculi Lbas Pvt.Ltd.",),
+        html.H2(children="March Data Analytics",),
+        html.P(
+            children=""
+            ""
+            " ",
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["Count"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Total Test in March"},
+            },
+        ),
+    ]
+)
+if __name__ == "__main__":
+    server=app.server
