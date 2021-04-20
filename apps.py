b0cbc08de91ada0c54abb0581a1df1d427560722
Yogesh Kumar Mangal <42840043+yogeshkumarmangal@users.noreply.github.com>
2021-04-21 01:01:24 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -3,6 +3,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import gunicorn
 data=pd.read_csv("Dashboards.csv")
 data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
 data.sort_values("Date", inplace=True)
